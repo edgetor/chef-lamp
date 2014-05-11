a06174e8758ece39b80361437e33aed752391143
EdgeTor <me@edgetor.com>
2014-05-10 21:10:27 -0400

    Added phpmyadmin unattended installation

--- metadata.rb
+++ metadata.rb
@@ -1,14 +1,18 @@
 name              "lamp"
 maintainer        "Edge Tor"
 maintainer_email  "me@edgetor.com"
-description       "Chef Cookbook to install Apache2.4, PHP5.5, Composer, and setup VirtualHost"
+description       "Chef Cookbook to install Apache2.4, PHP5.5, Composer, phpMyAdmin, and setup VirtualHost"
 version           "1.0.0"
 
-recipe "server", "Chef Cookbook to install Apache2.4, PHP5.5, Composer, and setup VirtualHost"
+recipe "default", "Chef Cookbook to install Apache2.4, PHP5.5, Composer, phpMyAdmin, and setup VirtualHost"
 
 depends "apache"
 depends "apt"
+depends "chef-php-extra"
 depends "npm"
+depends "openssl"
+depends "php"
+
 
 %w{ debian ubuntu }.each do |os|
   supports os
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,10 +1,10 @@
 #
 # Cookbook Name:: lamp
-# Recipe:: server
+# Recipe:: default
 #
 
 # Necessary Recipes
-include_recipe "apt" 
+include_recipe "apt"
 include_recipe "npm"
 
 #Install APT & NPM Packages defined in Vagrant File
@@ -55,9 +55,27 @@ bash "composer" do
   EOH
 end
 
+# phpMyAdmin
+# Read Database Password from Vagrant File
+database_pwd = "#{node['server']['db_password']}"
+
+# Unattended phpmyadmin installation
+bash "phpmyadmin" do
+  code <<-EOH
+    sudo apt-get install -q -y debconf-utils
+    export DEBIAN_FRONTEND=noninteractive
+    echo 'phpmyadmin phpmyadmin/dbconfig-install boolean true' | sudo debconf-set-selections
+    echo 'phpmyadmin phpmyadmin/app-password-confirm password #{database_pwd}' | sudo debconf-set-selections
+    echo 'phpmyadmin phpmyadmin/mysql/admin-pass password #{database_pwd}' | sudo debconf-set-selections
+    echo 'phpmyadmin phpmyadmin/mysql/app-pass password #{database_pwd}' | sudo debconf-set-selections
+    echo 'phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2' | sudo debconf-set-selections
+    sudo apt-get install -q -y phpmyadmin
+    sudo sed -i '$ a\Include /etc/phpmyadmin/apache.conf' /etc/apache2/apache2.conf
+    sudo apache2ctl restart
+  EOH
+end
 
 #VHost Configuration
-
 #Disable Default config file and restart Apache service
 execute "a2dissite default" do 
   user "root"
@@ -76,5 +94,3 @@ web_app app_name + ".local" do
   docroot node['server']['docroot']
   log_dir node['apache']['log_dir'] 
 end
-
-
